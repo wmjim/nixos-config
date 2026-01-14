3574a2ca088c873dff3ae28923c012ff7290324d
meng.wang <meng.w1016@outlook.com>
2026-01-15 01:15:06 +0800

    fish 配置

--- home/common/fish.nix
+++ home/common/fish.nix
@@ -1,5 +1,148 @@
 { config, pkgs, ... }:
 
 {
-  programs.fish.enable = true;
+  programs.fish = {
+    enable = true;
+
+    # 交互式 shell 启动时执行的命令
+    interactiveShellInit = ''
+      # 禁用 greeting 消息
+      set -g fish_greeting
+
+      # 设置编辑器
+      set -gx EDITOR hx
+
+      # 设置语言环境
+      set -gx LANG en_US.UTF-8
+      set -gx LC_ALL en_US.UTF-8
+
+      # 初始化 Fisher 插件管理器
+      if not functions -q fisher
+        set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+        curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+      end
+    '';
+
+    # Fish 插件管理（使用 Nix 包管理的插件）
+    plugins = [
+      # z - 智能目录跳转，根据频率学习
+      {
+        name = "z";
+        src = pkgs.fishPlugins.z;
+      }
+
+      # 彩色 man 文档显示
+      {
+        name = "colored-man-pages";
+        src = pkgs.fishPlugins.colored-man-pages;
+      }
+
+      # fzf - 模糊搜索工具集成
+      # {
+      #   name = "fzf";
+      #   src = pkgs.fishPlugins.fzf;
+      # }
+
+      # forgit - Git 交互式工具
+      # {
+      #   name = "forgit";
+      #   src = pkgs.fishPlugins.forgit;
+      # }
+    ];
+
+    # Fish 别名
+    shellAliases = {
+      # 常用命令别名
+      ll = "ls -la";
+      la = "ls -A";
+      l = "ls -CF";
+      ".." = "cd ..";
+      "..." = "cd ../..";
+
+      # Git 别名（可选，如果你在 .gitconfig 中已经配置了可以不重复）
+      gs = "git status";
+      ga = "git add";
+      gc = "git commit";
+      gp = "git push";
+      gl = "git log --oneline --graph --decorate";
+
+      # 其他实用别名
+      cat = "bat";
+      grep = "rg";
+      find = "fd";
+    };
+
+    # Fish 函数
+    functions = {
+      # 创建并进入目录
+      mkcd = ''
+        mkdir -p $argv[1] && cd $argv[1]
+      '';
+
+      # 快速备份文件
+      backup = ''
+        cp $argv[1] $argv[1].backup
+      '';
+
+      # 显示 Git 分支
+      git_branch = ''
+        git branch --show-current
+      '';
+
+      # 使用 fzf 浏览 Git 历史记录
+      git_log_fzf = ''
+        git log --graph --color=always --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" $argv | fzf --ansi --no-sort --reverse --tiebreak=index --toggle-sort=\`)
+      '';
+
+      # 使用 fzf 查找文件
+      find_file = ''
+        fd -t f -H -I | fzf --multi --preview 'bat --color=always {}'
+      '';
+    };
+
+    # 环境变量
+    shellInit = ''
+      # 设置 PATH（如果需要添加额外的路径）
+      # fish_add_path /path/to/bin
+
+      # fzf 配置（如果安装了）
+      if type -q fzf
+        set -gx FZF_DEFAULT_OPTS '--height 40% --layout=reverse --border --preview "bat --color=always {}" --preview-window=right:60%'
+        set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git'
+      end
+    '';
+  };
+
+  # 安装 fish 相关的实用工具和插件
+  home.packages = with pkgs; [
+    bat      # cat 的替代品，支持语法高亮
+    ripgrep  # grep 的替代品，更快
+    fd       # find 的替代品，更友好
+    fzf      # 模糊搜索工具，用于命令行增强
+
+    # 可选的额外工具
+    eza      # 更好的 ls 替代品（如果使用可以添加 alias ls=eza）
+    zoxide   # 更智能的 cd 替代品
+    jq       # JSON 处理工具
+    httpie   # 人性化的 HTTP 客户端
+  ];
+
+  # 配置 zoxide（智能目录跳转）
+  programs.zoxide = {
+    enable = true;
+    enableFishIntegration = true;
+    options = [ "--cmd cd" ];
+  };
+
+  # 配置 fzf
+  programs.fzf = {
+    enable = true;
+    enableFishIntegration = true;
+    defaultOptions = [
+      "--height 40%"
+      "--layout=reverse"
+      "--border"
+      "--inline-info"
+    ];
+  };
 }
