9913307b0e7fd5293807fbe2b13635dea571f0de
meng.wang <meng.w1016@outlook.com>
2026-03-26 19:05:36 +0800

    更新配置：精简中文输入法支持

--- modules/nixos/fcitx5/fcitx5.nix
+++ modules/nixos/fcitx5/fcitx5.nix
@@ -1,45 +1,14 @@
 {pkgs, ...}: {
   # 选择输入法类型为 fcitx5
   i18n.inputMethod = {
-    type = "fcitx5";
     enable = true;
+    type = "fcitx5";
   };
 
-  # 添加常用中文模块
-  i18n.inputMethod.fcitx5.addons = with pkgs; [
-    fcitx5-rime
-    qt6Packages.fcitx5-configtool
-    qt6Packages.fcitx5-chinese-addons # 拼音、五笔等
-  ];
-  i18n.inputMethod.fcitx5.waylandFrontend = true;
-
-  /*
-  *i18n.inputMethod.fcitx5.settings.inputMethod与catppuccin.fcitx5.apply相冲突
-  *因为两者都试图管理同一个配置文件
-  *catppuccin.fcitx5可以导入主题配色可以使用classicui.globalSection.Theme导入主题
-  *也可以禁用inputMethod，设置主题。
-  */
-
-  # 设置rime输入法与美式键盘英文输入法并排序rime第一位
-  i18n.inputMethod.fcitx5.settings.inputMethod = {
-    GroupOrder."0" = "Default";
-    "Groups/0" = {
-      Name = "Default";
-      "Default Layout" = "us";
-      DefaultIM = "keyboard-us";
-    };
-    "Groups/0/Items/0".Name = "keyboard-us";
-    "Groups/0/Items/1".Name = "rime";
-  };
-
-  i18n.inputMethod.fcitx5.settings = {
-    globalOptions = {
-      # "Behavior" = {
-      # 关键设置：关闭程序内预编辑，强制使用 Fcitx5 浮窗(能解决 neovide 中无法预编辑的问题)
-      # "PreeditEnabledByDefault" = "False";
-      # };
-      "Hotkey/TriggerKeys" = {"0" = "Super+space";};
-    };
-    # addons.classicui.globalSection.Theme = "catppuccin-mocha-mauve";
+  # 设置输入法环境变量
+  environment.sessionVariables = {
+    GTK_IM_MODULE = "fcitx";
+    QT_IM_MODULE = "fcitx";
+    XMODIFIERS = "@im=fcitx";
   };
 }
