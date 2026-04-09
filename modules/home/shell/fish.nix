31a2f320b9e6cfe42e5e3388d244779170dbf4d9
meng.wang <meng.wang@example.com>
2026-04-09 10:47:20 +0800

    更新 fish 配置

--- modules/home/shell/fish.nix
+++ modules/home/shell/fish.nix
@@ -5,10 +5,15 @@
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
-      # 启动时运行 fastfetch
-      if type -q fastfetch
-        fastfetch
-      end
+      # 禁用 greeting 消息
+      set fish_greeting ""
+
+      # 设置编辑器
+      set -gx EDITOR hx
+
+      # 设置语言环境
+      set -gx LANG zh_CN.UTF-8
+      set -gx LC_ALL zh_CN.UTF-8
 
       # 设置代理快捷命令
       function proxy
@@ -29,13 +34,46 @@
     '';
     shellAliases = {
       # 常用别名
+      ".." = "cd ..";
+      "..." = "cd ../..";
+      
+      ls = "eza";
       ll = "eza -la";
-      la = "eza -a";
+      la = "eza -A";
       lt = "eza --tree";
-      cat = "bat";
+
+      gs = "git status";
+      ga = "git add";
+      gc = "git commit";
+      gp = "git push";
+      gl = "git log --oneline --graph --decorate";
+
+      bat = "bat --style=plain";
+      cat="bat --style=plain --paging=never";
+
       cd = "z";
-      vim = "nvim";
-      vi = "nvim";
+      grep = "rg";
+      find = "fd";
+
+      vim = "hx";
+      vi = "hx";
+
+      cc  = "claude";
     };
+
+    # 环境变量
+    shellInit = ''
+      # 设置 PATH（如果需要添加额外的路径）
+      # fish_add_path /path/to/bin
+
+      # npm 全局模块路径
+      fish_add_path /home/mengw/.npm-global/bin
+
+      # fzf 配置（如果安装了）
+      if type -q fzf
+        set -gx FZF_DEFAULT_OPTS '--height 40% --layout=reverse --border --preview "bat --color=always {}" --preview-window=right:60%'
+        set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git'
+      end
+    '';
   };
 }
