b45aefa52295de479c3405d603ef04c2e0eae601
meng.wang <meng.w1016@outlook.com>
2026-05-08 20:32:43 +0800

    优化字体配置，移除不必要的字体，更新默认字体设置

--- hosts/_common/nixos/locale.nix
+++ hosts/_common/nixos/locale.nix
@@ -13,17 +13,15 @@
     maple-mono.NormalNL-NF-unhinted
     # 中文无衬线/屏幕阅读字体
     lxgw-wenkai-screen
-    noto-fonts-cjk-serif      # 思源宋体
+    sarasa-gothic
     # 英文衬线字体
     source-serif-pro          
     noto-fonts-color-emoji
-    # 中文无衬线回退
-    noto-fonts-cjk-sans       # 思源黑体
   ];
 
   fonts.fontconfig.defaultFonts = {
-    serif = [ "Source Serif Pro" "Noto Serif CJK SC" ];
-    sansSerif = [ "Noto Sans Mono CJK SC" ];
+    serif = [ "Source Serif Pro" "LXGW WenKai Screen"];
+    sansSerif = [ "Sarasa UI SC" ];
     monospace = [ "Maple Mono Normal NL NF CN" ];
     emoji = [ "Noto Color Emoji" ];
   };
