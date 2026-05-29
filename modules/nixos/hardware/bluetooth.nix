d11d3c11297b57e4bc1c91ad64fb190bc506abb2
meng.wang <meng.w1016@outlook.com>
2026-05-30 00:55:56 +0800

    修复蓝牙无法启动

--- modules/nixos/hardware/bluetooth.nix
+++ modules/nixos/hardware/bluetooth.nix
@@ -4,4 +4,12 @@
 {
   # 蓝牙管理
   hardware.bluetooth.enable = true;
+  # 开机自启蓝牙适配器电源
+  hardware.bluetooth.powerOnBoot = true;
+  # 蓝牙固件
+  hardware.enableRedistributableFirmware = true;
+  # 阻止 ideapad_laptop 模块初始时屏蔽蓝牙
+  boot.extraModprobeConfig = ''
+    options ideapad_laptop rfkill_sw_state=1
+  '';
 }
