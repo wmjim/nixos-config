50a2bc3504b8ecd21eb61e2b35f1aafaf3692aed
meng.wang <meng.w1016@outlook.com>
2026-06-09 07:31:39 +0800

    为 desktop 添加适当配置

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -10,4 +10,31 @@
 
   # 主机名
   networking.hostName = "desktop";
+
+  # 虚拟化
+  virtualisation.libvirtd.enable = true;
+  boot.kernelParams = [ "console=ttyS0" ];
+
+  # ==========================================
+  # 为 nix-daemon 设置代理 (核心！)
+  # ==========================================
+  systemd.services.nix-daemon.environment = {
+    http_proxy = "http://127.0.0.1:7897";
+    https_proxy = "http://127.0.0.1:7897";
+    ftp_proxy = "http://127.0.0.1:7897";
+    no_proxy = "localhost,127.0.0.1,local.domain,192.168.0.0/16,bilibili.com,*.bilibili.com";
+  };
+
+  # ==========================================
+  # 系统级用户会话代理设置 (修正变量名)
+  # ==========================================
+  environment.sessionVariables = {
+    http_proxy = "http://127.0.0.1:7897";
+    https_proxy = "http://127.0.0.1:7897";
+    ftp_proxy = "http://127.0.0.1:7897";
+    no_proxy = "localhost,127.0.0.1,local.domain,192.168.0.0/16,bilibili.com,*.bilibili.com";
+  };
+
+  # RDP 防火墙端口
+  networking.firewall.allowedTCPPorts = [ 3389 ];
 }
