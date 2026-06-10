a4259bdb2b1e02ea78b98cee7b386c1bfeaa38fa
meng.wang <meng.w1016@outlook.com>
2026-06-10 22:36:12 +0800

    重构应用配置，添加 thunderbird、cc-switch 和 wechat 的文件重定向脚本

--- modules/nixos/gui-de/common/apps.nix
+++ modules/nixos/gui-de/common/apps.nix
@@ -25,7 +25,13 @@
     siyuan # 笔记
     xournalpp # PDF批注手写笔记
     zotero # 文献管理
-    thunderbird # 邮件管理
+    # thunderbird 所有文件重定向到 ~/files/apps/thunderbird
+    # 通过覆盖 $HOME 阻止在主目录生成 ~/thunderbird、~/.thunderbird
+    (pkgs.writeShellScriptBin "thunderbird" ''
+      TH_DIR="$HOME/files/apps/thunderbird"
+      mkdir -p "$TH_DIR"
+      exec env HOME="$TH_DIR" ${pkgs.thunderbird}/bin/thunderbird --profile "$HOME/files/apps/thunderbird" "$@"
+    '')
     obs-studio # 录屏
     anki # 助记卡片
     picgo # 图床管理
@@ -38,11 +44,21 @@
     '')
     folo # 信息聚合平台
     localsend # 跨平台文件共享
-    cc-switch # AI 管理
+    # cc-switch 所有文件重定向到 ~/files/apps/cc-switch
+    (pkgs.writeShellScriptBin "cc-switch" ''
+      CC_DIR="$HOME/files/apps/cc-switch"
+      mkdir -p "$CC_DIR"
+      exec env HOME="$CC_DIR" ${pkgs.cc-switch}/bin/cc-switch "$@"
+    '')
     cherry-studio
     baidupcs-go # 百度网盘
     # === 通讯工具 ===
-    wechat # 微信
+    # 微信所有文件重定向到 ~/files/apps/wechat
+    (pkgs.writeShellScriptBin "wechat" ''
+      WECHAT_DIR="$HOME/files/apps/wechat"
+      mkdir -p "$WECHAT_DIR"
+      exec env HOME="$WECHAT_DIR" ${pkgs.wechat}/bin/wechat "$@"
+    '')
     wemeet # 腾讯会议
     # qq # qq
     discord # Discord
