81b3e6063e5839bdf4fc2bccde7266e0f250684a
meng.wang <meng.w1016@outlook.com>
2026-06-14 21:10:41 +0800

    更新光标主题为 Bibata-Modern-Classic，并在 noctalia-greeter 中添加光标设置

--- modules/nixos/gui-de/niri/noctalia.nix
+++ modules/nixos/gui-de/niri/noctalia.nix
@@ -15,6 +15,13 @@
   programs.noctalia-greeter = {
     enable = true;
     package = inputs.noctalia-greeter.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    # 预选 niri 作为桌面会话
+    greeter-args = "--session niri"; 
+    settings.cursor = {
+      theme = "Bibata-Modern-Classic"; # 光标主题
+      size = 24; # 光标大小
+      package = pkgs.bibata-cursors; # 提供光标主题的包（必须，否则 greeter 找不到主题文件）
+    };
   };
 
   environment.systemPackages = [
