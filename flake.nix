44932df9200a6277f76b48745bc7c5e39d032787
meng.wang <meng.w1016@outlook.com>
2026-06-19 20:21:46 +0800

    更新 flake.lock 和 flake.nix，调整 nix-darwin 的版本以匹配 26.05

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
 
     # nix-darwin (macOS)
     nix-darwin = {
-      url = "github:LnL7/nix-darwin";
+      url = "github:LnL7/nix-darwin/nix-darwin-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
