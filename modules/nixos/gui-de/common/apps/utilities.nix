6b0c3214ec470290b0fc6dfa451e0c3892cfd712
meng.wang <meng.w1016@outlook.com>
2026-06-24 16:22:25 +0800

    优化：为欧陆词典添加窗口规则，修复 Wayland 兼容性

--- modules/nixos/gui-de/common/apps/utilities.nix
+++ modules/nixos/gui-de/common/apps/utilities.nix
@@ -1,6 +1,26 @@
 # 系统工具 / 实用程序
 { pkgs, ... }:
 
+let
+  # 欧陆词典（自带的 Qt 5.15 只有 xcb 平台插件，需要强制回退到 XWayland）
+  eudic-fixed = pkgs.symlinkJoin {
+    name = "eudic-fixed";
+    paths = [ pkgs.eudic ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/eudic \
+        --unset WAYLAND_DISPLAY \
+        --set QT_QPA_PLATFORM "xcb" \
+        --set QT_SCALE_FACTOR 1.5 \
+        --set GTK_IM_MODULE "fcitx" \
+        --set QT_IM_MODULE "fcitx" \
+        --set XMODIFIERS "@im=fcitx" \
+        --unset QT_STYLE_OVERRIDE \
+        --set XKB_CONFIG_ROOT "${pkgs.xkeyboard_config}/share/X11/xkb" \
+        --set GST_PLUGIN_SYSTEM_PATH_1_0 ""
+    '';
+  };
+in
 {
   environment.systemPackages = with pkgs; [
     ddcutil # 显示器亮度调节
@@ -15,6 +35,6 @@
     # cc-switch # AI 管理工具
     cherry-studio
     baidupcs-go # 百度网盘
-    eudic # 欧陆词典
+    eudic-fixed # 欧陆词典（修复 Wayland 兼容性）
   ];
 }
