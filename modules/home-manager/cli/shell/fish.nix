ba937171213bcff7cfa81a08f9af5634073815c1
meng.wang <meng.w1016@outlook.com>
2026-09-16 22:48:31 +0800

    feat: fish 集成 fzf.fish 与 decors/fish-colored-man
    fzf.fish 用 nixpkgs 的 fishPlugins.fzf-fish（本仓库锁定的 home-manager
    plugins 只接受 { name, src }，不能直接传包）；decors/fish-colored-man
    未入 nixpkgs，该插件整体即一个 man 函数，直接用 programs.fish.functions
    托管。顺带在 cheatsheet 补全 fzf.fish v11 实际默认绑定（Ctrl-Alt-F/L/S/P、
    Ctrl-V），此前按旧版文档写的 Ctrl-T / Alt-C 并不存在。

--- modules/home-manager/cli/shell/fish.nix
+++ modules/home-manager/cli/shell/fish.nix
@@ -15,6 +15,62 @@ in
   config = lib.mkIf (cfg.enable && shellCfg.enable && cliCfg.enable) {
     programs.fish = {
       enable = true;
+      plugins = [
+        # 默认绑定: Ctrl-Alt-F 目录搜索 / Ctrl-R 历史 / Ctrl-Alt-L git log /
+        # Ctrl-Alt-S git status / Ctrl-Alt-P 进程 / Ctrl-V 变量
+        # 本仓库锁定的 home-manager 要求 { name, src }，不能直接传包
+        {
+          name = "fzf-fish";
+          src = pkgs.fishPlugins.fzf-fish.src;
+        }
+      ];
+      # decors/fish-colored-man 未入 nixpkgs，该插件整体即这一个函数，直接托管
+      # 上游: https://github.com/decors/fish-colored-man/blob/master/functions/man.fish
+      functions.man = {
+        wraps = "man";
+        description = "Format and display manual pages";
+        body = ''
+          set -q man_blink; and set -l blink (set_color $man_blink); or set -l blink (set_color -o red)
+          set -q man_bold; and set -l bold (set_color $man_bold); or set -l bold (set_color -o 5fafd7)
+          set -q man_standout; and set -l standout (set_color $man_standout); or set -l standout (set_color 949494)
+          set -q man_underline; and set -l underline (set_color $man_underline); or set -l underline (set_color -u afafd7)
+
+          set -l end (printf "\e[0m")
+
+          set -lx LESS_TERMCAP_mb $blink
+          set -lx LESS_TERMCAP_md $bold
+          set -lx LESS_TERMCAP_me $end
+          set -lx LESS_TERMCAP_so $standout
+          set -lx LESS_TERMCAP_se $end
+          set -lx LESS_TERMCAP_us $underline
+          set -lx LESS_TERMCAP_ue $end
+          set -lx LESS '-R -s'
+
+          set -lx GROFF_NO_SGR yes # fedora
+
+          set -lx MANPATH (string join : $MANPATH)
+          if test -z "$MANPATH"
+            type -q manpath
+            and set MANPATH (command manpath)
+          end
+
+          # Check data dir for Fish 2.x compatibility
+          set -l fish_data_dir
+          if set -q __fish_data_dir
+            set fish_data_dir $__fish_data_dir
+          else
+            set fish_data_dir $__fish_datadir
+          end
+
+          set -l fish_manpath (dirname $fish_data_dir)/fish/man
+          if test -d "$fish_manpath" -a -n "$MANPATH"
+            set MANPATH "$fish_manpath":$MANPATH
+            command man $argv
+            return
+          end
+          command man $argv
+        '';
+      };
       interactiveShellInit = ''
         set fish_greeting ""
         # 设置终端为英文环境
